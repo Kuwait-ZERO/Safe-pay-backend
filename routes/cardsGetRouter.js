bd89929014913f0a30dbc20767bce5d2914fc509
Hussain <hussein-_2000@hotmail.com>
2024-12-08 14:09:27 +0300

    get cards that made by user

--- routes/cardsGetRouter.js
+++ routes/cardsGetRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const Card = require("../model/card");
+const { requireAuth, validateRequest } = require("../middleware");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const cards = await Card.find();
+router.get("/", requireAuth, validateRequest, async (req, res) => {
+  const cards = await Card.find({ user: req.user.id });
   res.json(cards);
 });
 
